eb3ec8c9297d147af43184863b0e1c20d5a048e9
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-17 17:17:07 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,7 @@
 def position_taken?(board,index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
   end
 end
